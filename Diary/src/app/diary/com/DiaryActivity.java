97db09f2102424298192c70fa85481636ef8418f
Ivan <ivanp@kth.se>
2012-03-16 08:04:51 +0100

    first commit

---
+++ Diary/src/app/diary/com/DiaryActivity.java
@@ -0,0 +1,13 @@
+package app.diary.com;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DiaryActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
